a2eb75e910f5b84c24460bd7b910d40a9b445cd2
johnnyclapham <jmclapham@email.wm.edu>
2022-04-29 13:22:12 -0400

    #3 and tool outputs are now contained withing txt files

--- casa.py
+++ casa.py
@@ -9,58 +9,62 @@ import time
 # os.system('ls')
 import time
 
+def printToolSelection(cryptoguard_flag,flowdroid_flag,quark_flag):
+    print(" _____________________________"
+          "\n Successful invocation of CASA. Printing configuration."
+          "\n --> cryptoguard flag set: {} "
+          "\n --> quark flag set:       {} "
+          "\n --> flowdroid flag set:   {} "
+          "\n _____________________________".format(
+        cryptoguard_flag,
+        flowdroid_flag,
+        quark_flag
+    ))
 
-def main(cryptoguard_flag, flowdroid_flag, qark_flag):
-    # print "Hello World!"
 
-    # ----------------------------------
+def runTools(cryptoguard_flag,flowdroid_flag,quark_flag):
+    printToolSelection(cryptoguard_flag, flowdroid_flag, quark_flag)
 
     if (cryptoguard_flag == "1"):
-        # print "@@@@@@@@@@@@@@@@@@@@@@"
-        # print "@@@@@@@@@@@@@@@@@@@@@@"
         print "cryptoguard is running"
-        # java -jar src/cryptoguard/cryptoguard.jar -in apk -s apks/air.pokemonquiz.apk
         os.system('java -jar src/cryptoguard/cryptoguard.jar '
                   '-in apk '
-                  '-s apks/air.pokemonquiz.apk')
+                  '-s apks/air.pokemonquiz.apk '
+                  '-m SX'
+                  '>output/cryptoguard-terminal-output.txt  2>&1')
     else:
         print "no cryptoguard"
 
-    # ----------------------------------
-
-    if (qark_flag == "1"):
-        # print "@@@@@@@@@@@@@@@@@@@@@@"
-        # print "@@@@@@@@@@@@@@@@@@@@@@"
+    if (flowdroid_flag == "1"):
         print "qark is running"
         os.system(
             'python2 src/qark/qark-0.9-alpha.1/qark.py '
             '-p apks/air.pokemonquiz.apk '
             '--source=1 '
-            '--exploit=0 ')
-        # '-t acceptterms | -b src/flowdroid/android-sdk-macosx')
-        # '-t acceptterms | -b android-sdk_r24.0.2-macosx/android-sdk-macosx/platforms')
-
-        # time.sleep(5)
-        # print "yam"
-        # os.system('n')
-        # time.sleep(5)
-        # os.system('android-sdk_r24.0.2-macosx/')
+            '--exploit=0 '
+            '> output/qark-terminal-output.txt')
     else:
         print "no qark"
 
-    # ----------------------------------
-
-    if (flowdroid_flag == "1"):
-        # print "@@@@@@@@@@@@@@@@@@@@@@"
-        # print "@@@@@@@@@@@@@@@@@@@@@@"
+    if (quark_flag == "1"):
         print "flowdroid is running"
         os.system(
             'java -jar src/flowdroid/soot-infoflow-cmd-2.9.0-jar-with-dependencies.jar '
             '-a apks/air.pokemonquiz.apk '
             '-p src/flowdroid/android-sdk-macosx/platforms/ '
-            '-s src/flowdroid/SourcesAndSinksSusi.txt')
+            '-s src/flowdroid/SourcesAndSinksSusi.txt'
+            '>output/flowdroid-terminal-output.txt  2>&1')
     else:
         print "no flowdroid"
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-cg", "--cryptoguard", help="cryptoguard flag")
+    parser.add_argument("-qk", "--quark", help="quark flag")
+    parser.add_argument("-fd", "--flowdroid", help="flowdroid flag")
+    args = parser.parse_args()
+
+    runTools(args.cryptoguard, args.flowdroid, args.quark)
 
     print "___________________________"
     print "CASA has finished. Goodbye!"
@@ -68,21 +72,4 @@ def main(cryptoguard_flag, flowdroid_flag, qark_flag):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-cg", "--cryptoguard", help="cryptoguard flag")
-    parser.add_argument("-qk", "--quark", help="quark flag")
-    parser.add_argument("-fd", "--flowdroid", help="flowdroid flag")
-    args = parser.parse_args()
-
-    print(" _____________________________"
-          "\n Successful invocation of CASA. Printing configuration."
-          "\n --> cryptoguard flag set: {} "
-          "\n --> quark flag set:       {} "
-          "\n --> flowdroid flag set:   {} "
-          "\n _____________________________".format(
-        args.cryptoguard,
-        args.flowdroid,
-        args.quark
-    ))
-
-    main(args.cryptoguard, args.flowdroid, args.quark)
+    main()
