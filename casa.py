bf6db3c44aae9f0675d1707429ec7a6f6d99cfa9
johnnyclapham <jmclapham@email.wm.edu>
2022-05-07 14:05:35 -0400

    evaluation has been completed -> output exists  in ../casa_output.txt // also large refactoring #3

--- casa.py
+++ casa.py
@@ -1,38 +1,146 @@
 # Note: This is the runner for our comparison
 # Please see README.md for individual execution
+import os
+
 from casaUtilities import *
 import argparse
-from tqdm import tqdm
+from tqdm.auto import tqdm
+import multiprocessing
+import time
+from os.path import exists
+from multiprocessing import Process
 
 
-def print_tool_selection(cryptoguard_flag, quark_flag, flowdroid_flag, apk_name):
+
+def print_tool_selection(cryptoguard_flag, qark_flag, flowdroid_flag, apk_name):
     print(" _____________________________"
           "\n Successful invocation of CASA. Printing configuration."
           "\n --> cryptoguard flag set: {} "
-          "\n --> quark flag set:       {} "
+          "\n --> qark flag set:       {} "
           "\n --> flowdroid flag set:   {} "
           "\n --> APK to scan:   {} "
           "\n _____________________________".format(
         cryptoguard_flag,
-        quark_flag,
+        qark_flag,
         flowdroid_flag,
         apk_name
     ))
 
 
-def runToolsOnAPK(cryptoguard_flag, quark_flag, flowdroid_flag, apk_name):
+def run_cryptoguard(apk_name):
+    os.system('java -jar src/cryptoguard/cryptoguard.jar '
+              '-in apk '
+              '-s apks/' + apk_name + ' -m SX ' +
+              '>output/cryptoguard/terminal/cryptoguard-terminal-output-'
+              + apk_name + '.txt  2>&1')
+
+
+def run_qark(apk_name):
+    path_to_file = 'output/qark/terminal/qark-terminal-output-' + apk_name + '.txt'
+    path_to_report_file = 'output/qark/reports/report-' + apk_name
+    if exists(path_to_report_file):
+        print("skipping apk: "+apk_name)
+    else:
+        os.system(
+            'python2 src/qark/qark-0.9-alpha.1/qark.py '
+            '-p apks/' + apk_name + ' ' +
+            '--source=1 ' + '--exploit=0 ' + ' -report-type=json'
+            + '&> output/qark/terminal/qark-terminal-output-' + apk_name + '.txt')
+
+    # os.system(
+    #     'python2 src/qark/qark-0.9-alpha.1/qark.py '
+    #     '-p apks/' + apk_name + ' ' +
+    #     '--source=1 ' + '--exploit=0 ' + ' -report-type=json')
+
+
+def run_flowdroid(apk_name):
+    os.system(
+        'java -jar src/flowdroid/soot-infoflow-cmd-2.9.0-jar-with-dependencies.jar '
+        '-a apks/' + apk_name + ' ' +
+        '-p src/flowdroid/android-sdk-macosx/platforms/ ' +
+        '-s src/flowdroid/SourcesAndSinksSusi.txt' +
+        '> output/flowdroid/terminal/flowdroid-terminal-output-' +
+        apk_name + '.txt  2>&1')
+
+
+def run_tools_on_apk(cryptoguard_flag, qark_flag, flowdroid_flag, apk_name):
     # Note: Print which tools the user has selected to execute
-    print_tool_selection(cryptoguard_flag, quark_flag, flowdroid_flag, apk_name)
+    # print_tool_selection(cryptoguard_flag, qark_flag, flowdroid_flag, apk_name)
 
     # Note: If the flag is active, execute the tool
+    if flowdroid_flag == "1":
+        print "flowdroid is running"
+        try:
+            # p = multiprocessing.Process(target=run_flowdroid, name="run_flowdroid",
+            #                             args=(apk_name,))
+            # p.start()
+            # # Note: Wait for number_seconds
+            # timeout_seconds = 30
+            # time.sleep(timeout_seconds)
+            # p.terminate()
+            # p.join()
+
+            # run_flowdroid(apk_name)
+
+            p = Process(target=run_flowdroid, args=(apk_name,))
+            # timeout = 180
+            timeout = 720
+            p.start()
+            counter = 0
+            while p.is_alive():
+                time.sleep(1)
+                counter += 1
+                print(str(counter) + ' seconds have passed')
+                if counter > timeout :
+                    print "Child process consumed too much run-time. Going to kill it!"
+                    os.kill(p.pid)
+                    break
+
+            # os.system(
+            #     'java -jar src/flowdroid/soot-infoflow-cmd-2.9.0-jar-with-dependencies.jar '
+            #     '-a apks/' + apk_name + ' ' +
+            #     '-p src/flowdroid/android-sdk-macosx/platforms/ ' +
+            #     '-s src/flowdroid/SourcesAndSinksSusi.txt' +
+            #     '> output/flowdroid/terminal/flowdroid-terminal-output-' +
+            #     apk_name + '.txt  2>&1')
+        except:
+            print('flowdroid failed to run')
+    else:
+        print "flowdroid is not running"
+
     if cryptoguard_flag == "1":
         print "cryptoguard is running"
         try:
-            os.system('java -jar src/cryptoguard/cryptoguard.jar '
-                      '-in apk '
-                      '-s apks/' + apk_name + ' -m SX ' +
-                      '>output/cryptoguard/terminal/cryptoguard-terminal-output-'
-                      + apk_name + '.txt  2>&1')
+            # p = multiprocessing.Process(target=run_cryptoguard, name="run_cryptoguard",
+            #                             args=(apk_name,))
+            # p.start()
+            # # Note: Wait for number_seconds
+            # timeout_seconds = 240
+            # time.sleep(timeout_seconds)
+            # p.terminate()
+            # p.join()
+
+            # run_cryptoguard(apk_name)
+
+            p = Process(target=run_cryptoguard, args=(apk_name,))
+            # timeout = 180
+            timeout = 720
+            p.start()
+            counter = 0
+            while p.is_alive():
+                time.sleep(1)
+                counter += 1
+                print(str(counter) + ' seconds have passed')
+                if counter > timeout :
+                    print "Child process consumed too much run-time. Going to kill it!"
+                    os.kill(p.pid)
+                    break
+
+            # os.system('java -jar src/cryptoguard/cryptoguard.jar '
+            #           '-in apk '
+            #           '-s apks/' + apk_name + ' -m SX ' +
+            #           '>output/cryptoguard/terminal/cryptoguard-terminal-output-'
+            #           + apk_name + '.txt  2>&1')
         except:
             print('cryptoguard failed to run')
         # Note: Move report files into output directory
@@ -41,66 +149,110 @@ def runToolsOnAPK(cryptoguard_flag, quark_flag, flowdroid_flag, apk_name):
     else:
         print "cryptoguard is not running"
 
-    if quark_flag == "1":
+    if qark_flag == "1":
         print "qark is running"
-        try:
-            os.system(
-                'python2 src/qark/qark-0.9-alpha.1/qark.py '
-                '-p apks/' + apk_name + ' ' +
-                '--source=1 ' + '--exploit=0 ' + ' -report-type=json'
-                + '&> output/qark/terminal/qark-terminal-output-' + apk_name + '.txt')
-        except:
-            print('qark failed to run')
-        move_qark_report(apk_name)
+        path_to_file = 'output/qark/terminal/qark-terminal-output-' + apk_name + '.txt'
+        if exists(path_to_file):
+
+            print("skipping apk: "+apk_name)
+        else:
+
+            try:
+                p = multiprocessing.Process(target=run_qark, name="run_qark",
+                                            args=(apk_name,))
+                # p.start()
+                # # Note: Wait for number_seconds
+                # timeout_seconds = 180
+                # time.sleep(timeout_seconds)
+                # p.terminate()
+                # p.join()
+
+                p = Process(target=run_qark, args=(apk_name,))
+                # timeout = 180
+                timeout = 720
+                p.start()
+                counter = 0
+                while p.is_alive():
+                    time.sleep(1)
+                    counter += 1
+                    print(str(counter) + ' seconds have passed')
+                    if counter > timeout :
+                        print "Child process consumed too much run-time. Going to kill it!"
+                        os.kill(p.pid)
+                        break
+
+
+
+                # run_qark(apk_name)
+                # timeout_seconds = 60
+                # time.sleep(timeout_seconds)
+                # os.system('c')
+
+                # os.system(
+                #     'python2 src/qark/qark-0.9-alpha.1/qark.py '
+                #     '-p apks/' + apk_name + ' ' +
+                #     '--source=1 ' + '--exploit=0 ' + ' -report-type=json'
+                #     + '&> output/qark/terminal/qark-terminal-output-' + apk_name + '.txt')
+                # os.system('y')
+            except:
+                print('qark failed to run')
+            move_qark_report(apk_name)
     else:
         print "qark is not running"
 
-    if flowdroid_flag == "1":
-        print "flowdroid is running"
-        try:
-            os.system(
-                'java -jar src/flowdroid/soot-infoflow-cmd-2.9.0-jar-with-dependencies.jar '
-                '-a apks/' + apk_name + ' ' +
-                '-p src/flowdroid/android-sdk-macosx/platforms/ ' +
-                '-s src/flowdroid/SourcesAndSinksSusi.txt' +
-                '> output/flowdroid/terminal/flowdroid-terminal-output-' +
-                apk_name + '.txt  2>&1')
-        except:
-            print('flowdroid failed to run')
-    else:
-        print "flowdroid is not running"
+    #
+
+    # if flowdroid_flag == "1":
+    #     print "flowdroid is running"
+    #     try:
+    #         os.system(
+    #             'java -jar src/flowdroid/soot-infoflow-cmd-2.9.0-jar-with-dependencies.jar '
+    #             '-a apks/' + apk_name + ' ' +
+    #             '-p src/flowdroid/android-sdk-macosx/platforms/ ' +
+    #             '-s src/flowdroid/SourcesAndSinksSusi.txt' +
+    #             '> output/flowdroid/terminal/flowdroid-terminal-output-' +
+    #             apk_name + '.txt  2>&1')
+    #     except:
+    #         print('flowdroid failed to run')
+    # else:
+    #     print "flowdroid is not running"
 
 
-def main():
-    # Note: Get arguments for which tools we wish to execute
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-cg", "--cryptoguard", help="cryptoguard flag")
-    parser.add_argument("-qk", "--quark", help="quark flag")
-    parser.add_argument("-fd", "--flowdroid", help="flowdroid flag")
-    args = parser.parse_args()
-
+def execute_tools(args):
     # Note: For each application in Set A, scan using tools in Set T
     # Note: Using tqdm for progress bar -> github.com/tqdm/tqdm
-    for file_name in tqdm(os.listdir("./apks/")):
+    for file_name in tqdm(os.listdir("./apks/"), position=0, leave=True):
         if file_name.endswith('apk'):
             # Note: We scan each APK
             apk_name = file_name
+            print "\n --> APK to scan:   {}  \r".format(apk_name)
             # Note: Perform analysis on APK using our set T of tools
             # TODO: Uncomment below when we want to re-enable tool execution!
-            # runToolsOnAPK(args.cryptoguard, args.quark, args.flowdroid, apk_name)
-            # Note: After tools have run, directories are cleaned for next execution
+            run_tools_on_apk(args.cryptoguard, args.qark, args.flowdroid, apk_name)
 
-    # TODO: Parse the reports from tool output
-    casa_output_file = 'casa_output.txt'
+            # p = multiprocessing.Process(target=run_tools_on_apk, name="run_tools_on_apk",
+            #                             args=(args.cryptoguard, args.qark, args.flowdroid, apk_name,))
+            # p.start()
+            # # Note: Wait for number_seconds
+            # timeout_seconds = 240
+            # time.sleep(timeout_seconds)
+            # p.terminate()
+            # p.join()
+
+
+def parse_reports(casa_output_file):
+    success_count = 0
+    fail_count = 0
 
     # Note: erase casa_output_file for a fresh output log
     with open(casa_output_file, 'w') as filetowrite:
         filetowrite.write('CASA has extracted outputs.\n\n')
 
-
     # Note: For each apk in 'apks/'
     for file_name in tqdm(os.listdir("./apks/")):
         if file_name.endswith('apk'):
+            # Note: We parse the output of each apk on each specified tool
+            apk_name = file_name
             with open(casa_output_file, 'a') as filetowrite:
                 filetowrite.write('\n\n\n\n\n\n')
                 filetowrite.write('\n========================')
@@ -108,22 +260,120 @@ def main():
                 filetowrite.write('\n========================\n')
                 filetowrite.write('Input APK: ' + apk_name + '\n')
                 filetowrite.write('------------------------\n')
+            try:
+                # # Note: We parse the output of each apk on each specified tool
+                # apk_name = file_name
+                # with open(casa_output_file, 'a') as filetowrite:
+                #     filetowrite.write('\n\n\n\n\n\n')
+                #     filetowrite.write('\n========================')
+                #     filetowrite.write('\n========================')
+                #     filetowrite.write('\n========================\n')
+                #     filetowrite.write('Input APK: ' + apk_name + '\n')
+                #     filetowrite.write('------------------------\n')
 
-        # Note: We parse the output of each apk on each specified tool
-            apk_name = file_name
-            tool_name = "CryptoGuard"
-            parse_output_by_apk_and_tool(apk_name, tool_name, casa_output_file)
-            with open(casa_output_file, 'a') as filetowrite:
-                filetowrite.write('------------------------\n')
-            tool_name = "QARK"
-            parse_output_by_apk_and_tool(apk_name, tool_name, casa_output_file)
-            with open(casa_output_file, 'a') as filetowrite:
-                filetowrite.write('------------------------\n')
-            tool_name = "FlowDroid"
-            parse_output_by_apk_and_tool(apk_name, tool_name, casa_output_file)
+                # # Note: We parse the output of each apk on each specified tool
+                # apk_name = file_name
 
-            with open(casa_output_file, 'a') as filetowrite:
-                filetowrite.write('------------------------\n')
+                tool_name = "CryptoGuard"
+                parse_output_by_apk_and_tool(apk_name, tool_name, casa_output_file)
+                with open(casa_output_file, 'a') as filetowrite:
+                    filetowrite.write('------------------------\n')
+
+                tool_name = "QARK"
+                parse_output_by_apk_and_tool(apk_name, tool_name, casa_output_file)
+                with open(casa_output_file, 'a') as filetowrite:
+                    filetowrite.write('------------------------\n')
+
+                tool_name = "FlowDroid"
+                parse_output_by_apk_and_tool(apk_name, tool_name, casa_output_file)
+                with open(casa_output_file, 'a') as filetowrite:
+                    filetowrite.write('------------------------\n')
+
+                success_count += 1
+            except:
+                print("failed to parse APK: " + apk_name)
+                parse_failed(casa_output_file)
+                fail_count += 1
+    print("APK CASA reports successfully generated:  " + str(success_count))
+    print("APK CASA reports with errors:  " + str(fail_count))
+
+
+def main():
+    # Note: Get arguments for which tools we wish to execute
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-cg", "--cryptoguard", help="cryptoguard flag")
+    parser.add_argument("-qk", "--qark", help="qark flag")
+    parser.add_argument("-fd", "--flowdroid", help="flowdroid flag")
+    args = parser.parse_args()
+
+    # print_tool_selection()
+    print_tool_selection(args.cryptoguard, args.qark, args.flowdroid, " ")
+
+    # Note: Execute our runner script to automatically evaluate our APK
+    #       using all tools
+    # execute_tools(args)  # TODO: Comment/ Uncomment as needed for testing
+
+    # Note: Specify which output text file to write casa report to
+    casa_output_file = 'casa_output.txt'
+    parse_reports(casa_output_file)
+
+    # # Note: For each application in Set A, scan using tools in Set T
+    # # Note: Using tqdm for progress bar -> github.com/tqdm/tqdm
+    # for file_name in tqdm(os.listdir("./apks/"), position=0, leave=True):
+    #     if file_name.endswith('apk'):
+    #         # Note: We scan each APK
+    #         apk_name = file_name
+    #         print("\n --> APK to scan:   {}  \r").format(apk_name)
+    #         # Note: Perform analysis on APK using our set T of tools
+    #         # TODO: Uncomment below when we want to re-enable tool execution!
+    #         # run_tools_on_apk(args.cryptoguard, args.qark, args.flowdroid, apk_name)
+    #
+    #         p = multiprocessing.Process(target=run_tools_on_apk, name="run_tools_on_apk", args=(args.cryptoguard, args.qark, args.flowdroid, apk_name,))
+    #         p.start()
+    #         # Note: Wait for 4 minutes
+    #         time.sleep(240)
+    #         p.terminate()
+    #         p.join()
+    # Note: Wait for 4 minutes
+    # time.sleep(240)
+    # p.terminate()
+    # p.join()
+
+    # # Note: After tools have run, directories are cleaned for next execution
+    #
+    # # TODO: Parse the reports from tool output
+    # casa_output_file = 'casa_output.txt'
+    #
+    # # Note: erase casa_output_file for a fresh output log
+    # with open(casa_output_file, 'w') as filetowrite:
+    #     filetowrite.write('CASA has extracted outputs.\n\n')
+    #
+    # # Note: For each apk in 'apks/'
+    # for file_name in tqdm(os.listdir("./apks/")):
+    #     if file_name.endswith('apk'):
+    #         with open(casa_output_file, 'a') as filetowrite:
+    #             filetowrite.write('\n\n\n\n\n\n')
+    #             filetowrite.write('\n========================')
+    #             filetowrite.write('\n========================')
+    #             filetowrite.write('\n========================\n')
+    #             filetowrite.write('Input APK: ' + apk_name + '\n')
+    #             filetowrite.write('------------------------\n')
+    #
+    #         # Note: We parse the output of each apk on each specified tool
+    #         apk_name = file_name
+    #         tool_name = "CryptoGuard"
+    #         parse_output_by_apk_and_tool(apk_name, tool_name, casa_output_file)
+    #         with open(casa_output_file, 'a') as filetowrite:
+    #             filetowrite.write('------------------------\n')
+    #         tool_name = "QARK"
+    #         parse_output_by_apk_and_tool(apk_name, tool_name, casa_output_file)
+    #         with open(casa_output_file, 'a') as filetowrite:
+    #             filetowrite.write('------------------------\n')
+    #         tool_name = "FlowDroid"
+    #         parse_output_by_apk_and_tool(apk_name, tool_name, casa_output_file)
+    #
+    #         with open(casa_output_file, 'a') as filetowrite:
+    #             filetowrite.write('------------------------\n')
     # Note: End CASA
     print "CASA has finished. Goodbye!"
     return 0
--- casaUtilities.py
+++ casaUtilities.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import xml.etree.ElementTree as ET
 
+
 def clean_directory(apk_name):
     for fileName in os.listdir("./"):
         if fileName.startswith("_C"):
@@ -34,56 +35,127 @@ def move_qark_report(apk_name):
               " folder if you would like to repeat.")
         shutil.rmtree(source_dir)
 
+def parse_failed(casa_output_file):
+    with open(casa_output_file, 'a') as filetowrite:
+        filetowrite.write('-> Failed to parse tools\n')
+        # for BugCategory in root.iter('BugCategory'):
+        #     filetowrite.write('    -> ' + str(BugCategory.attrib) + '\n')
+    print("parse fail message printed")
 
-def parse_output_by_apk_and_tool(apk_name, tool_name,casa_output_file):
-    print("parsing outputs for apk: "+apk_name)
+
+def parse_cryptoguard(apk_name, casa_output_file):
+    target_directory = 'output/cryptoguard/reports'
+    target_xml = target_directory + '/' + 'cryptoguard-' + apk_name + '.xml'
+    tree = ET.parse(target_xml)
+    root = tree.getroot()
+    with open(casa_output_file, 'a') as filetowrite:
+        filetowrite.write('-> Tool: CryptoGuard\n')
+        for BugCategory in root.iter('BugCategory'):
+            filetowrite.write('    -> ' + str(BugCategory.attrib) + '\n')
+    print("-> CryptoGuard output parsed")
+
+
+def parse_qark(apk_name, casa_output_file):
+    terminal_location = "output/qark/terminal/qark-terminal-output-" + apk_name + ".txt"
+    with open(casa_output_file, 'a') as filetowrite:
+        filetowrite.write('-> Tool: QARK\n')
+        with open(terminal_location) as file:
+            for line in file:
+                # Note: Scan log for warnings
+                # TODO: Uncomment if we want to include warnings
+                # if (line.rstrip().startswith("WARNING")):
+                #     finding = line.rstrip()
+                #     character_length_desired = 100
+                #     filetowrite.write('-> ' + finding[0:character_length_desired] + '\n')
+
+                # Note: Scan log for vulnerabilities
+                if (line.rstrip().startswith("POTENTIAL VULNERABILITY")):
+                    finding = line.rstrip()
+                    character_length_desired = 100
+                    filetowrite.write('    -> ' + finding[0:character_length_desired] + '\n')
+
+    print("-> qark output parsed")
+
+
+def parse_flowdroid(apk_name, casa_output_file):
+    terminal_location = "output/flowdroid/terminal/flowdroid-terminal-output-" + apk_name + ".txt"
+    with open(casa_output_file, 'a') as filetowrite:
+        filetowrite.write('-> Tool: QARK\n')
+        with open(terminal_location) as file:
+            for line in file:
+                pass
+            last_line = line
+            # Note: In FlowDroid, the last line of the terminal output
+            #       provides the summary of "leaks found"
+            if last_line.startswith("[main] INFO soot.jimple.infoflow.android.SetupApplication - Found"):
+                filetowrite.write('    -> ' + last_line + '\n')
+            else:
+                error_message = "N/A FlowDroid failed"
+                filetowrite.write('    -> ' + error_message + '\n')
+    print("-> FlowDroid output parsed")
+
+
+def parse_output_by_apk_and_tool(apk_name, tool_name, casa_output_file):
+    print("parsing outputs for apk: " + apk_name)
+
+    # p = multiprocessing.Process(target=runToolsOnAPK, name="runToolsOnAPK",
+    #                             args=(args.cryptoguard, args.qark, args.flowdroid, apk_name,))
+    # p.start()
+    # # Note: Wait for number_seconds
+    # timeout_seconds = 240
+    # time.sleep(timeout_seconds)
+    # p.terminate()
+    # p.join()
 
     if tool_name == 'CryptoGuard':
+        parse_cryptoguard(apk_name, casa_output_file)
 
-        target_directory = 'output/cryptoguard/reports'
-        target_xml = target_directory + '/' + 'cryptoguard-' + apk_name + '.xml'
-        tree = ET.parse(target_xml)
-        root = tree.getroot()
-        with open(casa_output_file, 'a') as filetowrite:
-            filetowrite.write('-> Tool: ' + tool_name + '\n')
-            for BugCategory in root.iter('BugCategory'):
-                filetowrite.write('    -> '+str(BugCategory.attrib) + '\n')
-        print("-> CryptoGuard output parsed")
+        # target_directory = 'output/cryptoguard/reports'
+        # target_xml = target_directory + '/' + 'cryptoguard-' + apk_name + '.xml'
+        # tree = ET.parse(target_xml)
+        # root = tree.getroot()
+        # with open(casa_output_file, 'a') as filetowrite:
+        #     filetowrite.write('-> Tool: ' + tool_name + '\n')
+        #     for BugCategory in root.iter('BugCategory'):
+        #         filetowrite.write('    -> '+str(BugCategory.attrib) + '\n')
+        # print("-> CryptoGuard output parsed")
 
     if tool_name == 'QARK':
-        terminal_location = "output/qark/terminal/qark-terminal-output-"+apk_name+".txt"
-        with open(casa_output_file, 'a') as filetowrite:
-            filetowrite.write('-> Tool: ' + tool_name + '\n')
-            with open(terminal_location) as file:
-                for line in file:
-                    # Note: Scan log for warnings
-                    # TODO: Uncomment if we want to include warnings
-                    # if (line.rstrip().startswith("WARNING")):
-                    #     finding = line.rstrip()
-                    #     character_length_desired = 100
-                    #     filetowrite.write('-> ' + finding[0:character_length_desired] + '\n')
-
-                    # Note: Scan log for vulnerabilities
-                    if (line.rstrip().startswith("POTENTIAL VULNERABILITY")):
-                        finding = line.rstrip()
-                        character_length_desired = 100
-                        filetowrite.write('    -> ' + finding[0:character_length_desired] + '\n')
-
-        print("-> qark output parsed")
+        parse_qark(apk_name, casa_output_file)
+        # terminal_location = "output/qark/terminal/qark-terminal-output-"+apk_name+".txt"
+        # with open(casa_output_file, 'a') as filetowrite:
+        #     filetowrite.write('-> Tool: ' + tool_name + '\n')
+        #     with open(terminal_location) as file:
+        #         for line in file:
+        #             # Note: Scan log for warnings
+        #             # TODO: Uncomment if we want to include warnings
+        #             # if (line.rstrip().startswith("WARNING")):
+        #             #     finding = line.rstrip()
+        #             #     character_length_desired = 100
+        #             #     filetowrite.write('-> ' + finding[0:character_length_desired] + '\n')
+        #
+        #             # Note: Scan log for vulnerabilities
+        #             if (line.rstrip().startswith("POTENTIAL VULNERABILITY")):
+        #                 finding = line.rstrip()
+        #                 character_length_desired = 100
+        #                 filetowrite.write('    -> ' + finding[0:character_length_desired] + '\n')
+        #
+        # print("-> qark output parsed")
 
     if tool_name == 'FlowDroid':
-        terminal_location = "output/flowdroid/terminal/flowdroid-terminal-output-"+apk_name+".txt"
-        with open(casa_output_file, 'a') as filetowrite:
-            filetowrite.write('-> Tool: ' + tool_name + '\n')
-            with open(terminal_location) as file:
-                for line in file:
-                    pass
-                last_line = line
-                # Note: In FlowDroid, the last line of the terminal output
-                #       provides the summary of "leaks found"
-                if last_line.startswith("[main] INFO soot.jimple.infoflow.android.SetupApplication - Found"):
-                    filetowrite.write('    -> ' + last_line + '\n')
-                else:
-                    error_message = "N/A FlowDroid failed"
-                    filetowrite.write('    -> ' + error_message + '\n')
-        print("-> FlowDroid output parsed")
+        parse_flowdroid(apk_name, casa_output_file)
+        # terminal_location = "output/flowdroid/terminal/flowdroid-terminal-output-"+apk_name+".txt"
+        # with open(casa_output_file, 'a') as filetowrite:
+        #     filetowrite.write('-> Tool: ' + tool_name + '\n')
+        #     with open(terminal_location) as file:
+        #         for line in file:
+        #             pass
+        #         last_line = line
+        #         # Note: In FlowDroid, the last line of the terminal output
+        #         #       provides the summary of "leaks found"
+        #         if last_line.startswith("[main] INFO soot.jimple.infoflow.android.SetupApplication - Found"):
+        #             filetowrite.write('    -> ' + last_line + '\n')
+        #         else:
+        #             error_message = "N/A FlowDroid failed"
+        #             filetowrite.write('    -> ' + error_message + '\n')
+        # print("-> FlowDroid output parsed")
